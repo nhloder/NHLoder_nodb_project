90068658460285d69a15d73681963781f80c51f1
nhloder <nhloder@gmail.com>
2019-10-24 22:47:59 -0600

    stillnotdun

---
+++ src/components/AllTools.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class AllTools extends Component {
+    constructor() {
+        super();
+        this.state ={
+            
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default AllTools;
